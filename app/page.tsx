2edd5e89bd2622a912838ad6de2e8b75f7cb0942
apprakash <101494351+apprakash@users.noreply.github.com>
2023-06-20 17:05:23 +0530

    Api and functions added

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,95 @@ import Link from "next/link";
 import { FaGithub, FaTwitter } from "react-icons/fa";
 import { SiLinkedin } from "react-icons/si";
 import { useDropzone } from "react-dropzone";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useCompletion } from "ai/react";
+import { Upload } from "upload-js";
+
+interface ExtendedFile extends File {
+  preview: string;
+}
 
 export default function Home() {
+  const [files, setFiles] = useState<ExtendedFile[]>([]);
+  const [caption, setCaption] = useState("");
+  const [isLoad, setIsLoad] = useState(false);
+
+  const { complete, completion, setCompletion, isLoading } = useCompletion({
+    api: "/api/caption",
+  });
+
+  const upload = Upload({
+    apiKey: "public_12a1yDhFXdiwiqc5cp4roMGKbtde",
+  });
+
+  const uploadComplete = async (fileUrl: string) => {
+    var response = await axios.post("api/", {
+      url: fileUrl,
+    });
+    const result = response.data;
+    const caption = "Caption: ";
+    const captionIndex = result.indexOf(caption);
+    let extractedText;
+    if (captionIndex !== -1) {
+      extractedText = result.substring(captionIndex + caption.length).trim();
+    } else {
+      extractedText = result.trim();
+    }
+    setIsLoad(false);
+    setCaption(extractedText);
+    console.log("calling open ai ");
+    complete(extractedText);
+  };
+
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
       "image/*": [],
     },
     maxFiles: 1,
-    // onDrop: async (acceptedFiles) => {
-    //   setFiles(
-    //     acceptedFiles.map((file) =>
-    //       Object.assign(file, { preview: URL.createObjectURL(file) })
-    //     )
-    //   )
-    // setCompletion("")
-    // setIsLoad(true)
-    // const { fileUrl } = await upload.uploadFile(acceptedFiles[0])
-    // await uploadComplete(fileUrl)
-    // },
+    onDrop: async (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, { preview: URL.createObjectURL(file) })
+        )
+      );
+      setCompletion("");
+      setIsLoad(true);
+      const { fileUrl } = await upload.uploadFile(acceptedFiles[0]);
+      await uploadComplete(fileUrl);
+    },
   });
+
+  useEffect(() => {
+    return () => {
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    };
+  }, [files]);
+
+  const thumbs = files.map((file) => (
+    <div>
+      <img
+        src={file.preview}
+        onLoad={() => URL.revokeObjectURL(file.preview)}
+        alt={file.name}
+      />
+    </div>
+  ));
+
+  const formatApiResult = (result: any) => {
+    if (result) {
+      const formattedResult = result.replace(/-/g, "<p class='mb-5'>");
+      const lineSpacingStyle = {
+        lineHeight: "1.5",
+      };
+      return (
+        <div
+          style={lineSpacingStyle}
+          dangerouslySetInnerHTML={{ __html: formattedResult }}
+        />
+      );
+    }
+  };
 
   return (
     <main className="flex min-h-screen flex-col p-24 bg-gradient-to-r from-rose-100 to-teal-100">
@@ -39,6 +109,7 @@ export default function Home() {
           Simply upload your image, and let CaptionIt work its magic.{" "}
         </p>
       </div>
+
       <div className="flex items-center justify-center text-center h-full w-full">
         <div
           {...getRootProps()}
@@ -47,6 +118,41 @@ export default function Home() {
           <input {...getInputProps()} />
           <p>Drag and drop some files here or click to select files</p>
           <em>(Only *.jpeg and *.png images will be accepted)</em>
+        </div>
+      </div>
+
+      <div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginLeft: "100px",
+            width: "50%",
+            marginTop: "20px",
+          }}
+        >
+          <div>{thumbs}</div>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "50%",
+            marginRight: "50px",
+            marginTop: "20px",
+          }}
+        >
+          <div>
+            {isLoad ? (
+              <div>Generating image description...</div>
+            ) : (
+              <p> {caption}</p>
+            )}
+          </div>
+          <div>
+            <p>{formatApiResult(completion)}</p>
+          </div>
         </div>
       </div>
 

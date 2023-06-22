92f10505bfbc3989728bddda0e3a32295e59279e
apprakash <101494351+apprakash@users.noreply.github.com>
2023-06-22 16:30:03 +0530

    Progressive Web Application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,27 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Caption It',
-  description: 'The web app that generates captivating social media captions instantly when you upload your images.',
-}
+  title: "Caption It",
+  description:
+    "The web app that generates captivating social media captions instantly when you upload your images.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
